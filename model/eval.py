620defd2f716ed997bddab97dc1d99092848b203
andrewh965 <122482526+andrewh965@users.noreply.github.com>
2024-09-11 17:14:33 -0700

    evaluation update

--- model/eval.py
+++ model/eval.py
@@ -1,75 +1,85 @@
-import fasttext
-from sklearn.model_selection import StratifiedKFold
-from train import fit 
 import os
+import fasttext
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def removeFile(file_path):
-    try:
-        os.remove(file_path)
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-    except PermissionError:
-        print(f"Permission denied: unable to delete {file_path}.")
-    except Exception as e:
-        print(f"Error: {e}")
+# Specify the directory you want to iterate through
+directory = 'C:\\VisualStudio\\ecs171-project-main\\model\\'
+width = 40
+test_file = 'C:\\VisualStudio\\ecs171-project-main\\data\\val_0.19999999999999996.txt' 
 
-#might not be neccessary to include classifier path since we are retraining model every iteration
-def cross_validation(classifier_path='./classifier.model', fold=4, dataSet='../data/train_author_selection.txt'):
+def metrics(model,modelname):
+    actual_labels = []
+    predicted_labels = []
+    prob_labels = []
+
     
-    #dont really need will delete later
-    classifier = fasttext.load_model(classifier_path)
-    
-    with open(dataSet,'r') as file:
-        lines=file.readlines()
-    
-    labels = []
-    texts  = []
-    
-    #extract labels and text from file. Required for skf.split
-    for line in lines:
-        label,text = line.split(' ',1)
-        labels.append(label)
-        texts.append(text.strip())
+    # Read the test file line by line
+    with open(test_file, 'r') as f:
+        for line in f:
+            # Split the line into the label and text
+            split_lt = line.strip().split(' ', 1)
+              
+            # Predict the label for the text
+            predicted_label, prob = model.predict(split_lt[1])
         
-    #skf splits dataset into train and test
-    skf = StratifiedKFold(n_splits=fold, shuffle = True)
-    accuracies = []  #will delete l8r
-    report     = []  #records fold acc
+            # Store the actual and predicted labels
+            actual_labels.append(split_lt[0])
+            predicted_labels.append(predicted_label[0])
+            prob_labels.append(prob)
+   
+   
+    cm = confusion_matrix(actual_labels, predicted_labels) 
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Reds')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title(f'{modelname}Confusion Matrix')
+    plt.savefig(f'{modelname}.png')  
     
-    i = 1  #for printing asthetic
-    for train_index, valid_index in skf.split(texts, labels):
-        train_data = [(labels[i], texts[i]) for i in train_index]
-        valid_data = [(labels[i], texts[i]) for i in valid_index]
+    return classification_report(actual_labels, predicted_labels)
+
+
+def compare_premade_models():
+    #finding number of models for pretty printing
+    numberOfModels = 0
+    for filename in os.listdir(directory):
+        if filename.endswith('.model'):
+            numberOfModels+=1
+
+    modelsTested = 1
+    eval = ""
+    # Iterate through all files in the directory
+    for filename in os.listdir(directory):
         
-        #write temp files for fasttext training
-        #fasttext does not have continuing training on model
-            #possible alternative: save hyperparams and use on next fold?
+        filepath = os.path.join(directory, filename)
         
-         # Save the training data to a file
-        with open('./crossTempTr.txt', 'w') as f:
-            for label, text in train_data:
-                f.write(f"{label} {text}\n")
+        progress = (modelsTested / numberOfModels) * 100
+            # Create a progress bar
+        bar_length = 20
+        filled_length = int(round(bar_length * modelsTested / float(numberOfModels)))
         
-        # Save the validation data to a file
-        with open('./crossTempV.txt', 'w') as f:
-            for label, text in valid_data:
-                f.write(f"{label} {text}\n")
+        
+        # Check load/train model
+        if filename.endswith('.model'):
+            model = fasttext.load_model(filepath)
+            progressBars = '#'*filled_length+'-'*(bar_length-filled_length)
+            filename = filename.ljust(width)  
             
-        classifier = fasttext.train_supervised(input = './crossTempTr.txt')
-        accuracy = classifier.test('./crossTempV.txt') 
-
-        #remove temp files
-        removeFile('./crossTempTr.txt')
-        removeFile('./crossTempV.txt')
-        accuracies.append(accuracy[1])
-        report.append(f"Fold {i}: {accuracy[1]:.4f}")
-        i+=1
-
-    mean_accuracy = sum(accuracies) / len(accuracies)
-    print(report)
-    print(f"Mean Accuracy: {mean_accuracy}")
-    
-
-cross_validation(fold = 4)
-#8=D
+            modelsTested += 1 
+            print(f'Testing: {filename} progress: {progressBars} %{progress}',end='\r')
+           
+            report = metrics(model, filename)
+            result = model.test('../data/train_author_selection.txt')
+           
+            eval += f"{filename}\nP@1: {result[1]:.3f}  R@1: {result[2]:.3f}\n"
+            eval += report
+            eval += '\n'
+        
+    print("\n",end='\r')
+    print(eval)
+        
+compare_premade_models()
